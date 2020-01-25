59e7108aa1c76014e99cb1f7fc5bac74e1755e95
Hadyan Ahmad <hadyandev@gmail.com>
2020-01-25 23:26:45 +0700

    implement yajra datatables

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\User;
 
 class HomeController extends Controller
 {
@@ -24,5 +24,24 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+
+    public function usersList()
+    {
+        $usersQuery = User::query();
+
+        $start_date = (!empty($_GET["start_date"])) ? ($_GET["start_date"]) : ('');
+        $end_date = (!empty($_GET["end_date"])) ? ($_GET["end_date"]) : ('');
+
+        if ($start_date && $end_date) {
+
+            $start_date = date('Y-m-d', strtotime($start_date));
+            $end_date = date('Y-m-d', strtotime($end_date));
+
+            $usersQuery->whereRaw("date(users.created_at) >= '" . $start_date . "' AND date(users.created_at) <= '" . $end_date . "'");
+        }
+        $users = $usersQuery->select('*');
+        return datatables()->of($users)
+            ->make(true);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('users-list', 'HomeController@usersList');
